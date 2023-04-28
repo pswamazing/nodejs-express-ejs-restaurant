9ac769e927d9285619d0f193feeb8b225aa1ca14
pswamazing <pswamazing@gmail.com>
2023-04-28 12:55:08 +0900

    first commit

---
+++ routes/restaurant.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const uuid = require("uuid");
+
+const resData = require("../util/restaurant-data");
+
+const router = express.Router();
+
+router.get("/restaurants", function (req, res) {
+  let order = req.query.order;
+  let nextOrder = "desc";
+
+  if (order !== "asc" && order !== "desc") {
+    order = "asc";
+  }
+
+  if (order === "desc") {
+    nextOrder = "asc";
+  }
+
+  const storedRestaurants = resData.getStoredRestaurants();
+
+  storedRestaurants.sort(function (resA, resB) {
+    if (
+      (order === "asc" && resA.name > resB.name) ||
+      (order === "desc" && resB.name > resA.name)
+    ) {
+      return 1;
+    }
+    return -1;
+  });
+
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+    nextOrder: nextOrder,
+  });
+});
+
+router.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  const storedRestaurants = resData.getStoredRestaurants();
+
+  const findRestaurant = storedRestaurants.filter(function (restaurant) {
+    return restaurantId == restaurant.id;
+  });
+
+  if (findRestaurant.length == 0) {
+    return res.status(404).render("404");
+  }
+
+  res.render("restaurants-detail", {
+    restaurant: findRestaurant[0],
+  });
+});
+
+router.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+router.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  restaurant.id = uuid.v4();
+
+  const restaurants = resData.getStoredRestaurants();
+
+  restaurants.push(restaurant);
+
+  resData.storedRestaurants(restaurants);
+
+  res.redirect("/confirm");
+});
+
+router.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+module.exports = router;
